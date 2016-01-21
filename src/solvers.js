51ff6450077b795de91ab223ea1373b535f9a624
shanemcgraw <shanemcgraw17@gmail.com>
2016-01-20 16:35:14 -0800

    Find solution for nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,11 +14,65 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+var deepCopy = function(array){
+  var copied = [];
+
+  array.forEach(function(row, i){
+    copied.push([]);
+    row.forEach(function(element) {
+      copied[i].push(element);
+    });
+  });
+
+  return copied;
+};
+
+
+var newPossibility = function(numRooks, board, row, column) {
+  var solution;
+
+  board.togglePiece(row, column);
+  
+  if (board.hasAnyRooksConflicts()) {
+    return;
+  }
+
+  numRooks++;
+
+  if(numRooks === board.get("n")){
+    return board.rows();
+  }else{
+
+    //if no, create new possibility with updated board and updated column and row info
+    for(var i=row+1; i<board.get("n"); i++){
+      for(var j=0; j<board.get("n"); j++){
+        // debugger;
+        // console.log(board);
+        var matrix = deepCopy(board.rows());
+        solution = newPossibility(numRooks, new Board(matrix), i, j);
+        if(solution){
+          return solution;
+        }
+      }
+    }
+  }
+};
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution;
+  var numRooks = 0;
+  var board = new Board({ 'n':n });
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      var matrix = deepCopy(board.rows());
+      solution = newPossibility(numRooks, new Board(matrix), i, j);
+      if(solution){
+        console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+        return solution;
+      }
+    } 
+  } 
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
