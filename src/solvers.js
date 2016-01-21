7be5cd6164fe95e837c94394af39190237426d5c
shanemcgraw <shanemcgraw17@gmail.com>
2016-01-20 16:59:49 -0800

    Implement n-rooks count very slowly

--- src/solvers.js
+++ src/solvers.js
@@ -28,7 +28,7 @@ var deepCopy = function(array){
 };
 
 
-var newPossibility = function(numRooks, board, row, column) {
+var rookRecurse = function(numRooks, board, row, column) {
   var solution;
   if (row > -1) {
     board.togglePiece(row, column);
@@ -47,7 +47,7 @@ var newPossibility = function(numRooks, board, row, column) {
     for(var i=row+1; i<board.get("n"); i++){
       for(var j=0; j<board.get("n"); j++){
         var matrix = deepCopy(board.rows());
-        solution = newPossibility(numRooks, new Board(matrix), i, j);
+        solution = rookRecurse(numRooks, new Board(matrix), i, j);
         if(solution){
           return solution;
         }
@@ -63,7 +63,7 @@ window.findNRooksSolution = function(n) {
   // for (var i = 0; i < n; i++) {
   //   for (var j = 0; j < n; j++) {
   //     var matrix = deepCopy(board.rows());
-      solution = newPossibility(numRooks, board, -1, 0);
+      solution = rookRecurse(numRooks, board, -1, 0);
       if(solution){
         console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
         return solution;
@@ -74,10 +74,45 @@ window.findNRooksSolution = function(n) {
   return solution;
 };
 
+
+var rookCountRecurse = function(numRooks, board, row, column, allSolutions) {
+  var solution;
+  if (row > -1) {
+    board.togglePiece(row, column);
+    
+    if (board.hasAnyRooksConflicts()) {
+      return;
+    }
+
+    numRooks++;
+  }
+  if(numRooks === board.get("n")){
+    allSolutions.push( board.rows() );
+  }else{
+
+    //if no, create new possibility with updated board and updated column and row info
+    for(var i=row+1; i<board.get("n"); i++){
+      for(var j=0; j<board.get("n"); j++){
+        var matrix = deepCopy(board.rows());
+        rookCountRecurse(numRooks, new Board(matrix), i, j, allSolutions);
+        // if(solution){
+
+        // }
+      }
+    }
+  }
+};
+
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var allSolutions = [];
+  var solutionCount; 
 
+  var numRooks = 0;
+  var board = new Board({ 'n':n });
+  rookCountRecurse(numRooks, board, -1, 0, allSolutions);
+
+  solutionCount = allSolutions.length;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
